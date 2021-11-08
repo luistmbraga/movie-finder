1589b5285dc7c69ab946931d101b4ef506568b45
Luís Braga <luistmbraga@gmail.com>
2021-11-08 09:51:19 +0000

    Update home.tsx

--- screens/home/home.tsx
+++ screens/home/home.tsx
@@ -11,36 +11,34 @@ import {
   getMoviesByTitle,
   getMoviesByPagination,
 } from "../home/domain/use-cases/get-movies";
-import {
-  MovieSearchResult,
-  Movies,
-} from "../home/domain/entities/movie-result";
-import Pages  from "./components/pagination/index";
+import { Movies } from "../home/domain/entities/movie-result";
+import Pages from "./components/pagination/index";
 
 const HomeScreen: NextPage = () => {
   const [inputValue, setInputValue] = useState("");
-  const [state, setState] = useState<Movies>({movielist: [], totalresults: 0});
+  const [state, setState] = useState<Movies>({
+    movielist: [],
+    totalresults: 0,
+  });
   const [page, setPage] = useState(1);
 
   const handleChangeSearchInput = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-        setInputValue(event.target.value);
+    setInputValue(event.target.value);
   };
 
   const handleSearch = async (event: React.KeyboardEvent) => {
-    if (event.which === 13){
+    if (event.which === 13) {
       const res = await getMoviesByTitle(inputValue);
-      setState({movielist: res.movielist, totalresults: res.totalresults});
+      setState({ movielist: res.movielist, totalresults: res.totalresults });
     }
-  }
+  };
 
-  const handleChangePage = async (
-    val: number
-  ) => {
+  const handleChangePage = async (val: number) => {
     setPage(val);
     const res = await getMoviesByPagination(val, inputValue);
-    setState({movielist: res.movielist, totalresults: res.totalresults});
+    setState({ movielist: res.movielist, totalresults: res.totalresults });
   };
 
   return (
